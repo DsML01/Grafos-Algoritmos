93f43a0b011aa5d2948d7ae993db9e7d33bf5c99
davi <davimelovas61@gmail.com>
2024-10-07 23:03:03 -0300

    BFS

--- Outros/BFS/BFS.cpp
+++ Outros/BFS/BFS.cpp
@@ -2,6 +2,49 @@
 
 using namespace std;
 
+void bfs(vector<vector<int>> grafo, vector<int> &pai, int vertice_inicial, vector<int> &pre)
+{
+    int cont = 0;
+
+    vector <int> dist(grafo.size(), INT_MAX);
+
+    queue<int> fila;
+
+    fila.push(vertice_inicial);
+
+    dist[vertice_inicial] = 0;
+
+    //enquanto a fila não estiver vazia
+    while(!(fila.empty()))
+    {
+        //pega o primeiro elemento sem tirar da fila
+        int w = fila.front();
+
+        pre[w] = cont++;
+
+        fila.pop();
+
+        //for vizinho in grafo[w]
+        for(auto vizinho: grafo[w])
+        {
+            if(dist[w] + 1 < dist[vizinho])
+            {
+                dist[vizinho] = dist[w] + 1;
+                pai[vizinho] = w;
+                fila.push(vizinho);
+            }
+        }
+    }
+
+    cout << "distancia:" << endl;
+    for(int i = 1; i < dist.size(); i++)
+    {
+        cout << i << ": " << dist[i]<< endl;
+    }
+
+    cout << endl;
+}
+
 void print_grafo(vector<vector<int>> grafo)
 {
     for(int i = 0; i < grafo.size(); i++)
@@ -18,7 +61,7 @@ void print_grafo(vector<vector<int>> grafo)
 
 int main()
 {
-    int n, m, cont = 0;
+    int n, m;
 
     cin >> n >> m;
 
@@ -35,10 +78,23 @@ int main()
         cin >> u >> v;
 
         grafo[u].push_back(v);//poe na lista de adj do vertice que v é vizinho de u, adicionando-o ao fim da lista
-        grafo[v].push_back(u);
+        //grafo[v].push_back(u); //Com essa linha, indicaria que o grafo não é direcionado
     }
 
-    print_grafo(grafo);
+    //funcao para printar o grafo que foi lido
+    //print_grafo(grafo);
+
+    bfs(grafo, pai, 1, pre);
+
+    cout << "Está funcionando, mas tem algo de errado por aqui" << endl << endl;
+    
+    cout << "Valores do pai" << endl;
+
+    for(int i = 1; i < n + 1; i++) cout << pai[i] << endl;
+
+    cout << "Valores do pre" << endl;
+
+    for(int i = 1; i < n + 1; i++) cout << pre[i] << endl;
 
     return 0;
 }
